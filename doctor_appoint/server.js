a07f22cc9d1d449d2904961c1ef4d3fc46dd91c2
Shivam <shivamsri3821@gmail.com>
2026-04-14 18:49:11 +0530

    Updated backend and added vercel config

--- doctor_appoint/server.js
+++ doctor_appoint/server.js
@@ -22,7 +22,7 @@ const app = express()
 
 //middlewares
 app.use(cors({
-  origin: "http://localhost:5173", // frontend URL (Vite)
+  origin: process.env.FRONTEND_URL||"http://localhost:5173" , 
   credentials: true
 }));
 app.use(express.json())
